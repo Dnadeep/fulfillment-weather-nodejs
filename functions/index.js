1c08733e123bd2f22db8f5b296af393bc8eda8f6
Dnadeep <39623365+Dnadeep@users.noreply.github.com>
2018-07-05 14:52:40 +0530

    Update index.js

--- functions/index.js
+++ functions/index.js
@@ -17,7 +17,7 @@ const http = require('http');
 const functions = require('firebase-functions');
 
 const host = 'api.worldweatheronline.com';
-const wwoApiKey = '<ENTER_WWO_API_KEY_HERE>';
+const wwoApiKey = '<7a998690795a4a3490992051180507>';
 
 exports.dialogflowFirebaseFulfillment = functions.https.onRequest((req, res) => {
   // Get the city and date from the request
